5a9de130172f72433f82c3f4b3339d82cb2d89e5
Ivan Zolotarev <ivanze@gmail.com>
2024-04-02 14:49:14 -0700

    Renamed Map.py -> map.py

--- map.py
+++ map.py
@@ -1,4 +1,6 @@
 from emoji import emojize
+import os
+from time import sleep
 
 
 class Map:
@@ -7,6 +9,7 @@ class Map:
         self.height = height
         self.grid = [[Tile.get_tile("grass") for _ in range(length)] for _ in range(height)]
         self.grid_size = length * height
+        self.coordinates = [(x, y) for y in range(height) for x in range(length)]
 
 
 class Tile:
@@ -29,5 +32,14 @@ map.grid[0][0] = Tile.get_tile("vampire")
 map.grid[1][1] = Tile.get_tile("elf")
 map.grid[4][9] = Tile.get_tile("rock")
 
-for row in map.grid:
-    print("".join(row))
+count = 0
+while count < 10:
+    os.system("cls")
+    for row in map.grid:
+        print("".join(row))
+    count += 1
+    print(f"Round {count}")
+    sleep(1)
+
+
+
